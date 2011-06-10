67a33f713a75b75e4d3091072527f34a3a801a58
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2011-06-10 14:25:37 -0400

    It's 2011 now.

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Commonsense Computing'
-copyright = u'2009, Commonsense Computing Group'
+copyright = u'2011, Commonsense Computing Group'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
